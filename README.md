6ea5a3a253876dd32505c2b909910538a58b68cf
Ernest Leung <ernestleung52@gmail.com>
2022-11-01 15:00:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=ts_aiNbFjkg&list=PLg1kVg5P_o1ssWhvTcP6UtACTnJvkaJ0y&index=2&ab_channel=EternalProgramming
---
+++ src/Server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+import consola, { Consola } from 'consola';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
+
+import * as dotenv from 'dotenv';
